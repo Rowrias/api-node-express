d2b6a9c811fb2f85f85dd33d83ffdd7eed84881a
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-07-04 09:50:47 -0300

    01.Deixando nossa API resiliente
    - Tratando erro por id no autoresController.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 import 'dotenv/config'
 import app from './src/app.js'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import mongoose from "mongoose"
 
-// mongoose.connect("mongodb+srv://admin:<db_password>@cluster0.buvwgnd.mongodb.net/");
+// mongoose.connect("mongodb+srv://admin:<db_password>@cluster0.buvwgnd.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
 
 mongoose.connect(process.env.STRING_CONEXAO_DB);
 
