63e8e6694baa1d0817594916104b6e20aa2aa8f2
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-07-07 11:59:20 -0300

    02.Avançando o tratamento de erros
    - Refatorando o manipulador de erros e tratando a página 404.

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -21,9 +22,9 @@ class AutorController {
       const autorResultado = await autores.findById(id);
 
       if(autorResultado !== null) {
-        res.status(200).send(autorResultado)
+        res.status(200).send(autorResultado);
       } else {
-        res.status(400).send({message: "Id do Autor não localizado."})
+        next(new NaoEncontrado("Id do Autor não localizado."));
       }
     } catch (erro) {
       next(erro);
